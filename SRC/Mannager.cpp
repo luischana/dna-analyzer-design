81f47a0ddf08b82391b9b303fafbb6b27c0a18b1
chani lewis <z618149938@gmail.com>
2020-07-06 10:43:26 -0700

    add Mannager

--- SRC/Mannager.cpp
+++ SRC/Mannager.cpp
@@ -1,5 +1,18 @@
-//
-// Created by a on 7/6/20.
-//
-
 #include "Mannager.h"
+#include "IReader.h"
+#include "IWriter.h"
+#include "Factory.h"
+
+
+void Mannager::doAction()
+{
+    ConsoleRead reader;
+    ConsoleWriter writer;
+
+    Params params(reader.read());
+    Factory::create(params)->run(params);
+
+    writer.write(params.getParams()[0].c_str());
+    writer.write(params.getParams()[1].c_str());
+    writer.write(params.getParams()[2].c_str());
+}
--- SRC/Mannager.h
+++ SRC/Mannager.h
@@ -1,13 +1,12 @@
-//
-// Created by a on 7/6/20.
-//
-
 #ifndef SRC_MANNAGER_H
 #define SRC_MANNAGER_H
 
 
-class Mannager {
+#include "Params.h"
 
+class Mannager {
+public:
+    void doAction();
 };
 
 
