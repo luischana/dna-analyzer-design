2886e86a978ad98a845466d5170c4d2e06b03d60
chani lewis <z618149938@gmail.com>
2020-07-05 17:46:53 -0700

    add CreationCommand

---
+++ SRC/CreationCommand.cpp
@@ -0,0 +1,1 @@
+#include "CreationCommand.h"
---
+++ SRC/CreationCommand.h
@@ -0,0 +1,12 @@
+#ifndef SRC_CREATIONCOMMAND_H
+#define SRC_CREATIONCOMMAND_H
+
+
+#include "ICommand.h"
+
+class CreationCommand: public ICommand {
+
+};
+
+
+#endif //SRC_CREATIONCOMMAND_H
