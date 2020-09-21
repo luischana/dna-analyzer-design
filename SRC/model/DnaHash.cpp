c1675118a52bb51ae3eaaca472733188d4ce54c6
chani lewis <z618149938@gmail.com>
2020-09-21 15:54:49 -0700

    add functions findIdByName, isexistName, isexistId

--- SRC/model/DnaHash.cpp
+++ SRC/model/DnaHash.cpp
@@ -6,3 +6,18 @@ void DnaHash::add(DnaMetaData* dna)
     getNameMap().insert(std::pair<std::string, size_t>(dna->getName(), dna->getId()));
     getIDMap().insert(std::pair<size_t, DnaMetaData*>(dna->getId(), dna));
 }
+
+size_t DnaHash::findIdByName(const std::string& name)
+{
+    return m_nameMap[name];
+}
+
+bool DnaHash::isexistName(const std::string &name)
+{
+    return m_nameMap.find(name) != m_nameMap.end();
+}
+
+bool DnaHash::isexistId(size_t id)
+{
+    return m_IDMap.find(id) != m_IDMap.end();
+}
--- SRC/model/DnaHash.h
+++ SRC/model/DnaHash.h
@@ -12,10 +12,14 @@ public:
     std::map<std::string, size_t>&  getNameMap() { return m_nameMap; }
     std::map<size_t, DnaMetaData*>& getIDMap()   { return m_IDMap; }
 
+    size_t findIdByName(const std::string& name);
+
+    bool isexistName(const std::string& name);
+    bool isexistId(size_t id);
+
 private:
     std::map<std::string, size_t>  m_nameMap;
     std::map<size_t, DnaMetaData*> m_IDMap;
 };
-
 
 #endif //SRC_DNAHASH_H
