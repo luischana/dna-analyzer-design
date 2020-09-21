07a187c14b420291dca7b4952d6728cd110a7e82
chani lewis <z618149938@gmail.com>
2020-09-21 15:29:23 -0700

    add ManipulationCommand

---
+++ SRC/controler/command/ManipulationCommand.cpp
@@ -0,0 +1,1 @@
+#include "ManipulationCommand.h"
---
+++ SRC/controler/command/ManipulationCommand.h
@@ -0,0 +1,12 @@
+#ifndef SRC_MANIPULATIONCOMMAND_H
+#define SRC_MANIPULATIONCOMMAND_H
+
+
+#include "ICommand.h"
+
+class ManipulationCommand: public ICommand {
+
+};
+
+
+#endif //SRC_MANIPULATIONCOMMAND_H
