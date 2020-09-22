d8b72c71904cf3c9da922680a89d763cca552bbd
chani lewis <z618149938@gmail.com>
2020-09-22 04:41:46 -0700

    file organization

--- SRC/controler/command/save.cpp
+++ SRC/controler/command/save.cpp
@@ -1,48 +1,62 @@
 #include <sstream>
 #include "save.h"
 #include "../../view/write/TxtFileWriter.h"
+#include "../AuxiliaryFunc.h"
 
 
 Save::Save(const Params& params)
 {
-    if (params.getParams().size() < 1 || params.getParams().size() > 2)
+    if (params.getParams().empty() || params.getParams().size() > 2)
     {
         throw std::invalid_argument("INVALID NUMS OF ARGUMENTS");
     }
+
+    if (!(params.getParams()[0][0] == '@' || params.getParams()[0][0] == '#'))
+    {
+        throw std::invalid_argument("THE COMMAND DOESN'T KNOW WHICH DNA TO REFER\n");
+    }
 }
+
 
 void Save::run(const Params& params, DnaHash& dnaHash, IWriter& writer)
 {
-    std::string name;
+    size_t id;
+    std::string nameFile;
+    std::string nameDna;
 
-    if (params.getParams().size() == 1)
+    if (params.getParams()[0][0] == '@')
     {
-        const char* temp = params.getParams()[0].c_str();
-
-        if (params.getParams()[0][0] == '#')
+        if (!dnaHash.isexistName(params.getParams()[0].substr(1)))
         {
-            size_t id = 0;
-            std::istringstream(temp + 1) >> id;
-
-            name = dnaHash.getIDMap()[id]->getName();
-
-            TxtFileWriter write(name + ".rawdna");
-            write.write(dnaHash.getIDMap()[dnaHash.getNameMap()[name]]->getDnaSequence().castChar());
+            writer.write("name of DNA not found\n");
+            return;
         }
 
-        if (params.getParams()[0][0] == '@')
-        {
-            name = dnaHash.getIDMap()[dnaHash.getNameMap()[temp + 1]]->getName();
-
-            TxtFileWriter write(name + ".rawdna");
-            write.write(dnaHash.getIDMap()[dnaHash.getNameMap()[temp + 1]]->getDnaSequence().castChar());
-        }
+        id = dnaHash.findIdByName(params.getParams()[0].substr(1));
     }
 
     else
     {
-        TxtFileWriter write(params.getParams()[1]);
-        write.write(params.getParams()[0]);
+        id = castToSize(params.getParams()[0].substr(1));
+
+        if (!dnaHash.isexistId(id))
+        {
+            writer.write("id of DNA not found\n");
+            return;
+        }
     }
+
+    if (params.getParams().size() == 1)
+    {
+        nameFile = dnaHash.getIDMap()[id]->getName() + ".rawdna";
+    }
+
+    else
+    {
+        nameFile = params.getParams()[1];
+    }
+
+    TxtFileWriter write(nameFile);
+    write.write(dnaHash.findInIdMap(id)->getDnaSequence().castChar());
 }
 
